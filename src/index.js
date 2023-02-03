664699c1590345f302e48509cd3fed7ae7058e02
Dr.Holy <dr.holyblack@gmail.com>
2023-02-03 14:57:22 +0700

    add config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-//const Web3 = require('web3')
+const Web3 = require('web3')
 
-//const checkAndInstantiateWeb3 = () => {
-//  try {
-//    const web3 = new Web3('ws://rpc.ankr.com/eth')
-//  } catch(e) {
-//    console.error(e)
-//  }
-//}
+const checkAndInstantiateWeb3 = () => {
+  try {
+    const web3 = new Web3('https://rpc.ankr.com/eth')
+    console.log(web3.eth)
+    return web3
+  } catch(e) {
+    console.error(e)
+  }
+}
 
 const _ = require('lodash')
 const { printMe } = require('./print')
@@ -14,12 +16,16 @@ const { printMe } = require('./print')
 const component = () => {
   const element = document.createElement('div')
   const btn = document.createElement('button')
+  const code = document.createElement('code')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
   btn.innerHTML = 'Click me and check the console!'
   btn.onclick = printMe
   element.appendChild(btn)
+
+  code.innerHTML = checkAndInstantiateWeb3()
+  element.appendChild(code)
 
   return element
 }
